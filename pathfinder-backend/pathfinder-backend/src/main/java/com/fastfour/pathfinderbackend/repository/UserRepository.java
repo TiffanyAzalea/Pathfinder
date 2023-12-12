b0c37ec49967ed9bc0e393cba1cbd22651daad91
freyawaterbel <freyawaterbel@gmail.com>
2023-12-11 19:53:29 -0500

    2. create user repository and controller

--- pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/repository/UserRepository.java
+++ pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.fastfour.pathfinderbackend.repository;public interface UserRepository {
+package com.fastfour.pathfinderbackend.repository;
+
+import com.fastfour.pathfinderbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
