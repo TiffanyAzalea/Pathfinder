d8b04b63ec23d954c2e2f904b0553d3ccce95fec
SwethaParished <swethaparished@gmail.com>
2023-12-18 07:30:27 -0600

    Completed backend part of 'Edit and Delete User Account'

--- pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/controller/UserController.java
+++ pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/controller/UserController.java
@@ -16,15 +16,41 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping("/user")
-    User newUser(@RequestBody User newUser) { return userRepository.save(newUser);}
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
 
     @GetMapping("/user")
-    List<User> getAllUsers() { return userRepository.findAll(); }
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
     @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
-                .orElseThrow(()->new UserNotFoundException(id));
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setFirstName(newUser.getFirstName());
+                    user.setLastName(newUser.getLastName());
+                    user.setPassword(newUser.getPassword());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+            userRepository.deleteById(id);
+            return "User with id " + id + " has been deleted successfully.";
+    }
 }
--- pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/exception/UserNotFoundException.java
+++ pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.fastfour.pathfinderbackend.exception;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(Long id){
-        super("We could not find the requested user");
+        super("We could not find the requested user id " + id);
     }
 }
