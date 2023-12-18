372fb3ed8ba31be569a6869a7df22918cdfc92bd
Tiffany Hill <hill.tiffany365@gmail.com>
2023-12-18 17:15:58 -0500

    Register and Log In Form DTO added

---
+++ pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/model/dto/RegisterFormDTO.java
@@ -0,0 +1,14 @@
+package com.fastfour.pathfinderbackend.model.dto;
+
+public class RegisterFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
