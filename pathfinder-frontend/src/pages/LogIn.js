5a093bd0edeee412d676cb97649856c5b42ed2ec
Tiffany Hill <hill.tiffany365@gmail.com>
2023-12-19 20:49:33 -0500

    Log in form created with buttons and boxes.

--- pathfinder-frontend/src/pages/LogIn.js
+++ pathfinder-frontend/src/pages/LogIn.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 export default function LogIn() {
   return (
-    <div>Log In</div>
+    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
+        <div className='big-white p-3 rounded w-25'>
+            <form action="">
+                <div className="mb-3">
+                    <label htmlFor="username"><strong>Username</strong></label>
+                    <input type="username" placeholder='Enter Username' className='form-control rounded-0'/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password"><strong>Password</strong></label>
+                    <input type="password" placeholder='Enter Password' className='form-control rounded-0'/>
+                </div>
+                <button className='btn btn-success w-100'>Log In</button>
+                <p></p>
+                <button className='btn btn-default border w-100 bg-light'>Create Account</button>
+            </form>
+        </div>
+    </div>
+
+   
   )
 }
