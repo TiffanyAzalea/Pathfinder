55e83db94a7f0320af9967170a01d84599ae14ba
Tiffany Hill <hill.tiffany365@gmail.com>
2023-12-25 21:17:20 -0500

    "Added some javascript code to get form to connect"

--- pathfinder-frontend/src/pages/LogIn.js
+++ pathfinder-frontend/src/pages/LogIn.js
@@ -2,14 +2,23 @@
 import React, { useState } from 'react';
 
 export default function LogIn() {
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         
         try {
-            const response = await fetch('http://localhost:8080/api/login', {
+            const response = await fetch('http://localhost:8080/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -30,16 +39,16 @@ export default function LogIn() {
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
         <div className='big-white p-3 rounded w-25'>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="username"><strong>Username</strong></label>
-                    <input type="username" placeholder='Enter Username' className='form-control rounded-0'/>
+                    <input type="username" placeholder='Enter Username' className='form-control rounded-0' value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' className='form-control rounded-0'/>
+                    <input type="password" placeholder='Enter Password' className='form-control rounded-0' value={password} onChange={handlePasswordChange}/>
                 </div>
-                <button className='btn btn-success w-100'>Log In</button>
+                <button type='submit' className='btn btn-success w-100'>Log In</button>
                 <p></p>
                 <button className='btn btn-default border w-100 bg-light'>Create Account</button>
             </form>
