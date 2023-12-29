1b520402fa3452f4d215381e6d114cd3213c345f
anju-r <anjalirallabandi@gmail.com>
2023-12-29 15:26:20 -0600

    Post data from front end to back end through API

--- pathfinder-frontend/src/App.js
+++ pathfinder-frontend/src/App.js
@@ -13,6 +13,7 @@ import Corousal from './layout/Corousal';
 import CreateHike from './components/CreateHike';
 import EditUser from './users/EditUser';
 import UserHomePage from './pages/UserHomePage';
+import AllHikes from './pages/AllHikes';
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
           <Route exact path="/edituser" element={<EditUser />} />
           <Route exact path="/userhomepage" element={<UserHomePage />} />
           <Route exact path="/createhike" element={<CreateHike />} />
+          <Route exact path="/allhikes" element={<AllHikes/>} />
         </Routes>
       </BrowserRouter>
       {/* <NavbarForHome /> */}
