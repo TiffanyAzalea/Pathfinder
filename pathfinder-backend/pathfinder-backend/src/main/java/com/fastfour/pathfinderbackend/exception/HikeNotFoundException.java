09d2a90718c0d5f161a5b21d0e5a65ed1d5c7061
SwethaParished <swethaparished@gmail.com>
2024-01-09 03:59:40 -0600

    Aded delete hike on backend

--- pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/exception/HikeNotFoundException.java
+++ pathfinder-backend/pathfinder-backend/src/main/java/com/fastfour/pathfinderbackend/exception/HikeNotFoundException.java
@@ -2,6 +2,6 @@ package com.fastfour.pathfinderbackend.exception;
 
 public class HikeNotFoundException extends RuntimeException{
     public HikeNotFoundException(Long id){
-        super("We could not find the requested user");
+        super("We could not find the requested hike");
     }
 }
