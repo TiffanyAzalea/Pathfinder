13ddcb4672ee97d931e9746ef1006b99dab335e0
Tiffany Hill <hill.tiffany365@gmail.com>
2024-01-17 02:35:32 -0500

    Trail search filtering through CreateHike trails. No map functionality.

--- pathfinder-frontend/src/components/CreateHike.js
+++ pathfinder-frontend/src/components/CreateHike.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import NavbarBS from '../layout/NavbarBS';
+import Search from '../components/Search'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hpdHRpYWthc2F0dGkiLCJhIjoiY2xwenY1cmVtMTBzZDJrcW5yb2Y5cjRzNSJ9.SYzooukcLn0gjeS-VTjdgw';
 
@@ -23,41 +24,37 @@ export default function CreateHike() {
   const [comment, setComment] = useState();
   const [trailName, setTrailName] = useState();
   const [allComments, setAllComments] = useState();
+  const [filteredTrailResults, setFilteredTrailResults] = useState([]); //TH
   let newDate = new Date()
 
+  const handleSearchResults = (results) => {
+    setFilteredTrailResults(results); // Filtered trail results
+    // You may want to update the map results as well if needed
+    // setFilteredMapResults(results);
+  };
 
   const onChangeComment = (e) => {
     setComment(e.target.value);
   }
 
-  const onInputChange = (e) => {
-
-  }
   const onSubmit = async (e) => {
     e.preventDefault();
-    // await setHike({
-    //   trailName: feature.properties.TRAIL_NAME,
-    //   areaName: feature.properties.AREA_NAME,
-    //   walkable: feature.properties.WALKING,
-    //   bikable: feature.properties.BIKING,
-    //   distance: feature.properties.GIS_MILES,
-    //   date: hikeDate.toLocaleDateString()
-    // })
-    // console.log(hike);
-    await axios.post("http://localhost:8080/createhike", {
-      trailName: feature.properties.TRAIL_NAME,
-      areaName: feature.properties.AREA_NAME,
-      walkable: feature.properties.WALKING,
-      bikeFriendly: feature.properties.BIKING,
-      distance: feature.properties.GIS_MILES.toFixed(2),
-      date: hikeDate.toLocaleDateString()
+    if (feature && feature.properties) {
+      await axios.post("http://localhost:8080/createhike", {
+        trailName: feature.properties.TRAIL_NAME,
+        areaName: feature.properties.AREA_NAME,
+        walkable: feature.properties.WALKING,
+        bikeFriendly: feature.properties.BIKING,
+        distance: feature.properties.GIS_MILES.toFixed(2),
+        date: hikeDate.toLocaleDateString()
     })
     navigate("/userhomepage")
+  } else {
+    console.error("Feature or its properties are undefined");
   }
-
+  };
   const submitComment = async function (e) {
     e.preventDefault();
-
     await axios.post("http://localhost:8080/comments", {
       trailName: feature.properties.TRAIL_NAME,
       text: comment,
@@ -72,7 +69,6 @@ export default function CreateHike() {
       .catch(error => console.log(error))
   }
 
-
   useEffect(() => {
     if (map.current === null) {
       map.current = new mapboxgl.Map({
@@ -82,107 +78,99 @@ export default function CreateHike() {
         zoom: zoom,
       });
 
-
       map.current.on('move', () => {
         setLng(map.current.getCenter().lng.toFixed(4));
         setLat(map.current.getCenter().lat.toFixed(4));
         setZoom(map.current.getZoom().toFixed(2));
       });
 
-
       map.current.on('click', (event) => {
         const features = map.current.queryRenderedFeatures(event.point, {
           layers: ['mo-trails-parsed-filtered']
         });
         if (features.length > 0) {
-          const feature = features[0];
-          //setSelectedCoordinates(feature.geometry.coordinates)
-          setFeature(feature);
-          axios.get("http://localhost:8080/comments/" + feature.properties.TRAIL_NAME)
-            .then((response) => {
-              setAllComments(response.data);
-            })
-            .catch(error => console.log(error))
+          const clickedFeature = features[0];
+          const isFilteredTrail = filteredTrailResults.some(
+            (result) => result.properties.TRAIL_NAME === clickedFeature.properties.TRAIL_NAME
+          );
 
+          if (isFilteredTrail) {
+            setFeature(clickedFeature);
+            axios.get("http://localhost:8080/comments/" + clickedFeature.properties.TRAIL_NAME)
+              .then((response) => {
+                setAllComments(response.data);
+              })
+              .catch(error => console.log(error))
 
-
-          const popup = new mapboxgl.Popup({ offset: [0, -15] })
-            .setLngLat(feature.geometry.coordinates)
-            .setHTML(
-              `<h6>${feature.properties.TRAIL_NAME}</h6>
-              <p style="margin-bottom: 0" >${feature.properties.AREA_NAME}</p>
-              <p style="margin-bottom: 0" >${feature.properties.WALKING}</p>
-              <p style="margin-bottom: 0" >${feature.properties.BIKING}</p>
-              <p style="margin-bottom: 0" >${Math.round(feature.properties.GIS_MILES * 100) / 100} miles</p>
-          `
-            )
-            .addTo(map.current);
+            const popup = new mapboxgl.Popup({ offset: [0, -15] })
+              .setLngLat(clickedFeature.geometry.coordinates)
+              .setHTML(
+                `<h6>${clickedFeature.properties.TRAIL_NAME}</h6>
+                <p style="margin-bottom: 0" >${clickedFeature.properties.AREA_NAME}</p>
+                <p style="margin-bottom: 0" >${clickedFeature.properties.WALKING}</p>
+                <p style="margin-bottom: 0" >${clickedFeature.properties.BIKING}</p>
+                <p style="margin-bottom: 0" >${Math.round(feature.properties.GIS_MILES * 100) / 100} miles</p>
+              `
+              )
+              .addTo(map.current);
+          }
         }
       });
     }
-  });
+  }, [lng, lat, zoom, map, mapContainer, filteredTrailResults, setFeature, setAllComments]);
 
-
-  // Calender code
+  // Calendar code
   function changeValue(val) {
     changeHikeDate(val);
   }
 
   return (
-
     <div>
+      <Search onSearchResults={handleSearchResults} />
       <div className='homepagebutton'>
         <Link className="btn btn-primary" to="/userhomepage">Home page</Link>
       </div>
-      {/* <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div> */}
 
       <div ref={mapContainer} className="map-container" />
       <div className='split middle'>
-
-        {Object.keys(feature).length ? (
+        <h1>Trail Details</h1>
+        {Object.keys(feature).length || (filteredTrailResults.length > 0) ? (
           <form onSubmit={(e) => onSubmit(e)}>
             <div className='hike-details-table'>
-              <h6>{feature.properties.TRAIL_NAME}</h6>
-              <p>{feature.properties.AREA_NAME}</p>
-              <p>{feature.properties.WALKING}</p>
-              <p>{feature.properties.BIKING}</p>
-              <p>{Math.round(feature.properties.GIS_MILES * 100) / 100} miles</p>
+              <h6>{feature.properties?.TRAIL_NAME || filteredTrailResults[0]?.properties?.TRAIL_NAME}</h6>
+              <p>{feature.properties?.AREA_NAME || filteredTrailResults[0]?.properties?.AREA_NAME}</p>
+              <p>{feature.properties?.WALKING || filteredTrailResults[0]?.properties?.WALKING}</p>
+              <p>{feature.properties?.BIKING || filteredTrailResults[0]?.properties?.BIKING}</p>
+              <p>{feature.properties?.GIS_MILES ? Math.round(feature.properties.GIS_MILES * 100) / 100 + ' miles' : 'N/A'}</p>
               <p>{hikeDate.toLocaleDateString()}</p>
             </div>
             <button className="btn btn-primary" type='submit' value={"createHike"}>Create Hike</button>
           </form>
-          //<LogoutButton onClick={this.handleLogoutClick} />
         ) : (
           <div> </div>
         )}
-        {/* <button type='submit' value={"createHike"}>Create Hike</button> */}
-
       </div>
 
       <div className='calendar'>
         <Calendar onChange={changeValue} value={hikeDate} />
         <p>The selected date is - {hikeDate.toLocaleDateString()}</p>
       </div>
+
       <form onSubmit={(e) => submitComment(e)}>
         <div className='comments'>
           <textarea style={{ width: '30%', borderRadius: '0.25em' }} onChange={(e) => onChangeComment(e)}></textarea>
           <button type='submit'>Comment</button>
-
         </div>
       </form>
+
       <div className='comments'>
         <h6>Comments:</h6>
         {allComments?.map((comment, index) => (
-
           <div key={index}>
             <h6>{comment.createdBy.firstName} {comment.createdBy.lastName} - {comment.createdDate}</h6>
             <p>{comment.text}</p>
           </div>
         ))}
-
-
       </div>
     </div>
   );
---
+++ pathfinder-frontend/src/components/Search.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import JSONDATA from '../data/MO_Trails_geo.json'
+
+export default function Search({ onSearchResults }) {
+  const [trailDataArray, setTrailDataArray] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
+  
+
+  useEffect(() => {
+    const dataArray = Array.isArray(JSONDATA.features) ? JSONDATA.features : [];
+    setTrailDataArray(dataArray);
+  }, [JSONDATA.features]);
+
+  const handleInputChange = (event) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+    setIsTyping(newSearchTerm.length > 0);
+
+    const results = trailDataArray.filter((val) => {
+      const trailNameMatch =
+        val.properties.TRAIL_NAME &&
+        val.properties.TRAIL_NAME.toLowerCase().includes(newSearchTerm.toLowerCase());
+
+      const bikingMatch =
+        val.properties.BIKING &&
+        val.properties.BIKING.toLowerCase().includes(newSearchTerm.toLowerCase());
+
+      return trailNameMatch || bikingMatch;
+    });
+
+    onSearchResults(results); // Pass filtered results to parent component
+  };
+
+  return (
+    <div className='Search'>
+      <input type='text' placeholder='Search...' onChange={handleInputChange} />
+      {/* Display filtered results */}
+      {isTyping &&
+        trailDataArray
+          .filter((val) => {
+            const trailNameMatch =
+              val.properties.TRAIL_NAME &&
+              val.properties.TRAIL_NAME.toLowerCase().includes(searchTerm.toLowerCase());
+
+            const bikingMatch =
+              val.properties.BIKING &&
+              val.properties.BIKING.toLowerCase().includes(searchTerm.toLowerCase());
+
+            return trailNameMatch || bikingMatch;
+          })
+          .map((val, key) => (
+            <div key={key} className='trail'>
+              {/* <p>
+                {val.properties.TRAIL_NAME} Biking: {val.properties.BIKING}
+              </p> */}
+            </div>
+          ))}
+    </div>
+  );
+}
